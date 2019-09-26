901b3270bc2314fd2d05710f338931ad5bfbefd4
Joseph Hu <hujoseph99@gmail.com>
2019-09-26 06:48:24 -0400

    set up db connection and got server running

--- server.js
+++ server.js
@@ -1,2 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const config = require("config");
+
+const app = express();
+
+app.use(express.json());
+
+mongoose
+  .connect(config.get("dbUri"), { useNewUrlParser: true })
+  .then(() => console.log("db connected..."))
+  .catch(err => console.log(err));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT);
