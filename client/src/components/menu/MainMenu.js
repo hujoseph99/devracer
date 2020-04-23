ab690e401b63f56247cf9971b40d8287b1a2cd2b
Joseph Hu <hujoseph99@gmail.com>
2020-04-23 19:00:41 -0400

    Added github icon and link to footer

--- client/src/components/menu/MainMenu.js
+++ client/src/components/menu/MainMenu.js
@@ -1,8 +1,11 @@
 import React, { Component } from "react";
 
-import LoginModal from "./LoginModal";
-import RegisterModal from "./RegisterModal";
-import GuestModal from "./GuestModal";
+// import LoginModal from "./LoginModal";
+// import RegisterModal from "./RegisterModal";
+// import GuestModal from "./GuestModal";
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
 
 import "../../css/verticalCenter.css";
 import "../../css/MainMenu.css";
@@ -14,7 +17,9 @@ export default class MainMenu extends Component {
         <h1 className="header">Typers</h1>
         <div className="horizontal debug-border">
         </div>
-        <i className="fab fa-github"></i>
+        <a target="_blank" href="https://github.com/hujoseph99/typing">
+          <FontAwesomeIcon className="icon-color" icon={faGithub} size="lg"/>
+        </a>
       </div>
     );
   }
