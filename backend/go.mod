17f11002097bbe1a4dad822ab705d4d1a76286dd
Joseph Hu <hujoseph99@gmail.com>
2020-08-09 17:39:16 -0400

    removed old backend code

--- backend/go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/hujoseph99/typing
-
-go 1.14
-
-require go.mongodb.org/mongo-driver v1.3.2
--- backend/typingMongo/config.go
+++
@@ -1,3 +0,0 @@
-package typingMongo
-
-const mongoURI = "mongodb+srv://admin:random123@default-ofdlm.mongodb.net/test?retryWrites=true&w=majority"
