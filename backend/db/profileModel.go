69ddcd5bfb7c4aee3e4c60e43b9689dad3b418b2
Joseph Hu <hujoseph99@gmail.com>
2020-08-12 00:34:07 -0400

    created profileModel

---
+++ backend/db/profileModel.go
@@ -0,0 +1,33 @@
+package db
+
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+// ProfileModel is a model for user profile data
+type ProfileModel struct {
+	ID                primitive.ObjectID `bson:"_id", json:"_id"`
+	TotalWordsTyped   int                `bson:"totalWordsTyped", json:"totalWordsTyped"`
+	RacesCompleted    int                `bson:"racesCompleted", json:"racesCompleted"`
+	RacesWon          int                `bson:"racesWon", json:"racesWon"`
+	MaxTPM            float64            `bson:"maxTPM", json:"maxTPM"`
+	AverageTPMAllTime float64            `bson:"averageTPMAllTime", json:"averageTPMAllTime"`
+	AverageTPMLast10  float64            `bson:"averageTPMLast10", json:"averageTPMLast10"`
+}
+
+// NewProfile is a constructor for the ProfileModel
+func NewProfile(id primitive.ObjectID, totalWordsTyped, racesCompleted, racesWon int,
+	maxTPM, averageTPMAllTime, averageTPMLast10 float64) *ProfileModel {
+
+	res := &ProfileModel{
+		ID:                id,
+		TotalWordsTyped:   totalWordsTyped,
+		RacesCompleted:    racesCompleted,
+		RacesWon:          racesWon,
+		MaxTPM:            maxTPM,
+		AverageTPMAllTime: averageTPMAllTime,
+		AverageTPMLast10:  averageTPMLast10,
+	}
+
+	return res
+}
