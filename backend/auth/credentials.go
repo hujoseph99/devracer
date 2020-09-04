59f7a6d3c0c1442aa854106f11ee2126f13ab66f
Joseph Hu <hujoseph99@gmail.com>
2020-09-04 00:13:35 -0400

    added some code to create and verify state strings (UNTESTED)

---
+++ backend/auth/credentials.go
@@ -0,0 +1,13 @@
+package auth
+
+func getSecretStateString() string {
+	return secretStateString
+}
+
+func getGithubClientID() string {
+	return githubClientID
+}
+
+func getGithubClientSecret() string {
+	return githubClientSecret
+}
--- backend/go.mod
+++ backend/go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gorilla/mux v1.7.4
 	go.mongodb.org/mongo-driver v1.4.0
+	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d
 )
