b1ba4ab43bcbc8bfb46de5f2971b13e2c7fb8ef7
Joseph Hu <hujoseph99@gmail.com>
2021-02-25 18:27:49 -0500

    scrapping existing frontend

--- frontend/src/types/errorTypes.js
+++
@@ -1,2 +0,0 @@
-export const ERROR_REGISTER_FAIL = 1;
-export const ERROR_LOGIN_FAIL = 2;
--- frontend/src/types/pageTypes.js
+++
@@ -1,5 +0,0 @@
-export const MAIN_MENU = 1;
-export const LOGIN_PAGE = 2;
-export const REGISTER_PAGE = 3;
-export const RACE_PAGE = 4;
-export const PRACTICE_PAGE = 5;
