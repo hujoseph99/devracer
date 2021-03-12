a1be6adc980b75432b0ee7005b2b38062b5468a2
Joseph Hu <hujoseph99@gmail.com>
2021-03-11 21:46:14 -0500

    new practice race implemented graphql

---
+++ backend/graphql/queries/newPracticeRace.go
@@ -0,0 +1,17 @@
+package queries
+
+import "github.com/graphql-go/graphql"
+
+var newPracticeRace = graphql.NewObject(
+	graphql.ObjectConfig{
+		Name: "NewPracticeRace",
+		Fields: graphql.Fields{
+			"snippet": &graphql.Field{
+				Type: snippetType,
+			},
+			"timeLimit": &graphql.Field{
+				Type: graphql.Int,
+			},
+		},
+	},
+)
