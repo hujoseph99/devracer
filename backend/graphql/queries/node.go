83671172f7d84ddaeeee6eb947ffc7e195834992
Joseph Hu <hujoseph99@gmail.com>
2021-03-11 21:14:09 -0500

    added node interface for graphql

---
+++ backend/graphql/queries/node.go
@@ -0,0 +1,17 @@
+package queries
+
+import (
+	"github.com/graphql-go/graphql"
+)
+
+var node = graphql.NewInterface(
+	graphql.InterfaceConfig{
+		Name: "Node",
+		Fields: graphql.Fields{
+			"id": &graphql.Field{
+				Type: graphql.ID,
+				// TODO: Resolver - If following GUID thing, add __typename in front of given id
+			},
+		},
+	},
+)
