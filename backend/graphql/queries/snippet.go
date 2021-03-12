80efbd6bb24ddc1c0253eea4ca912bfa9b9c5333
Joseph Hu <hujoseph99@gmail.com>
2021-03-11 21:37:05 -0500

    finished snippet object graphql

---
+++ backend/graphql/queries/snippet.go
@@ -0,0 +1,20 @@
+package graphql
+
+import "github.com/graphql-go/graphql"
+
+var snippet = graphql.NewObject(
+	graphql.ObjectConfig{
+		Name: "Snippet",
+		Fields: graphql.Fields{
+			"id": &graphql.Field{
+				Type: graphql.ID,
+			},
+			"raceContent": &graphql.Field{
+				Type: graphql.String,
+			},
+			"wordCount": &graphql.Field{
+				Type: graphql.Int,
+			},
+		},
+	},
+)
