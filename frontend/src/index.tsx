024fe73220b2b7f1913f5b3b0ce95f7e37247883
Joseph Hu <hujoseph99@gmail.com>
2021-03-24 23:40:50 -0400

    added apollo client (for graphql) to application, got simply query working

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,6 +3,29 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { store } from "./app/store";
 import { Provider } from "react-redux";
+import { ApolloClient, gql, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: 'http://localhost:8080/graphql',
+  cache: new InMemoryCache()
+});
+
+client
+  .query({
+    query: gql`
+      query getPracticeRace {
+        practiceRace {
+          snippet {
+            id
+            raceContent
+            tokenCount
+          }
+          timeLimit
+        }
+      }
+    `
+  })
+  .then(result => console.log(result));
 
 ReactDOM.render(
   <React.StrictMode>
