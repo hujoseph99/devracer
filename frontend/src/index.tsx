8bd1b692246b0de0801ed36e5620d21d0e97fe83
Joseph Hu <hujoseph99@gmail.com>
2021-03-25 22:37:56 -0400

    got rid of apollo

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,29 +3,32 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { store } from "./app/store";
 import { Provider } from "react-redux";
-import { ApolloClient, gql, InMemoryCache } from "@apollo/client";
 
-const client = new ApolloClient({
-  uri: 'http://localhost:8080/graphql',
-  cache: new InMemoryCache()
-});
-
-client
-  .query({
-    query: gql`
-      query getPracticeRace {
-        practiceRace {
-          snippet {
-            id
-            raceContent
-            tokenCount
-          }
-          timeLimit
-        }
+const query = `
+  query getPracticeRace {
+    practiceRace {
+      snippet {
+        id
+        raceContent
+        tokenCount
       }
-    `
+      timeLimit
+    }
+  }
+`;
+
+fetch('http://localhost:8080/graphql', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+  },
+  body: JSON.stringify({
+    query
   })
-  .then(result => console.log(result));
+})
+.then(r => r.json())
+.then(data => console.log('data returned:', data));
 
 ReactDOM.render(
   <React.StrictMode>
