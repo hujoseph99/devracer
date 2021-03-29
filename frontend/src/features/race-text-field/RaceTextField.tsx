7e635b81119637af04ae8c403b38b63187e8fa23
Joseph Hu <hujoseph99@gmail.com>
2021-03-29 19:18:00 -0400

    added monaco editor

--- frontend/src/features/race-text-field/RaceTextField.tsx
+++ frontend/src/features/race-text-field/RaceTextField.tsx
@@ -1,7 +1,10 @@
 import { Paper, Typography } from '@material-ui/core';
 import React, { useEffect } from 'react'
+import MonacoEditor from 'react-monaco-editor';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchNewPracticeRace, selectSnippet } from './raceTextFieldSlice';
+
+
 
 export const RaceTextField = (): JSX.Element => {
 	const dispatch = useDispatch();
@@ -11,9 +14,30 @@ export const RaceTextField = (): JSX.Element => {
 		dispatch(fetchNewPracticeRace());
 	}, [dispatch])
 
+	// return (
+	// 	<Paper variant='outlined'>
+	// 		<Typography color='secondary'>{snippet.raceContent}</Typography>
+	// 	</Paper>
+	// );
 	return (
-		<Paper variant='outlined'>
-			<Typography color='secondary'>{snippet.raceContent}</Typography>
-		</Paper>
-	);
+		<MonacoEditor 
+			width="800"
+			height="600"
+			language="javascript"
+			value={snippet.raceContent}
+			theme="vs-dark"
+			options={{
+				minimap: {
+					enabled: false
+				},
+				scrollbar: {
+					vertical: 'hidden',
+					verticalHasArrows: false
+				},
+				overviewRulerLanes: 0,
+				hideCursorInOverviewRuler: true,
+				overviewRulerBorder: false
+			}}
+		/>
+	)
 };
