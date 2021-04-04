b6029bd564daf087c4d390f4b470768de0e6f0cc
Joseph Hu <hujoseph99@gmail.com>
2021-04-03 21:14:32 -0400

    removed monaco editor

--- frontend/src/features/race-text-field/RaceTextField.tsx
+++ frontend/src/features/race-text-field/RaceTextField.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
-import { MonacoEditor } from './MonacoEditor';
+// import { MonacoEditor } from './MonacoEditor';
 import { fetchNewPracticeRace, selectSnippet } from './raceTextFieldSlice';
 
 export const RaceTextField = (): JSX.Element => {
@@ -11,10 +11,8 @@ export const RaceTextField = (): JSX.Element => {
 		dispatch(fetchNewPracticeRace());
 	}, [dispatch])
 
-	// TODO: Figure out how to stop text highlighting on load ... it's because
-	// 	the text initially starts out as "" because have no gotten it back from backend
-	// Potential fix: Only mount the component when we have a value to put into it
 	return (
-		<MonacoEditor value={snippet.raceContent} />
+		// <MonacoEditor value={snippet.raceContent} />
+		<div></div>
 	)
 };
