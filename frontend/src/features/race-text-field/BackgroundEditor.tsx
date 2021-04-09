9afd78484f8a54da24fec6fb81772d7d477713f2
Joseph Hu <hujoseph99@gmail.com>
2021-04-08 20:11:09 -0400

    got editor looking like it's disabled (minus bracket pair highlighting)

--- frontend/src/features/race-text-field/BackgroundEditor.tsx
+++ frontend/src/features/race-text-field/BackgroundEditor.tsx
@@ -1,3 +1,4 @@
+import { Ace } from 'ace-builds';
 import React, { createRef, useEffect, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
@@ -7,14 +8,32 @@ import { selectSnippet } from './raceFieldSlice';
 export const BackgroundEditor = (): JSX.Element => {
 	const dispatch = useDispatch();
 	const snippet = useSelector(selectSnippet);
+	let editor: Ace.Editor | null = null
+
+	const handleLoad = (aceEditor: Ace.Editor) => {
+		editor = aceEditor;
+
+		// settings to make the text editor look like it's disabled, some of the things are
+		// not yet supported in typescript so have to ignore them
+		aceEditor.getSession().selection.on('changeSelection', function ()
+		{
+			aceEditor.getSession().selection.clearSelection();
+		});
+		// @ts-ignore
+		editor.commands.commmandKeyBinding={}
+		// @ts-ignore
+		editor.textInput.getElement().disabled=true
+		// @ts-ignore
+		aceEditor.renderer.$cursorLayer.element.style.display = "none";
+	}
 
 	return (
 		<AceEditor 
 			mode='plain_text'
 			value={snippet.raceContent}
-			readOnly={true}
 			highlightActiveLine={false}
-			ref={aceEditor}
+			readOnly={true}
+			onLoad={handleLoad}
 		/>
 	);
 }
