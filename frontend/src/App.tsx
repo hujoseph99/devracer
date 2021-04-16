1dff140e1bfe21a6369adb4c66e2d36a0d7b86d9
Joseph Hu <hujoseph99@gmail.com>
2021-04-15 23:19:09 -0400

    added routing logic for main menu and practice race

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,32 @@
 import React from 'react';
+import {
+	Route,
+	BrowserRouter as Router,
+	Switch,
+  Redirect
+} from 'react-router-dom';
+
 import { CssBaseline, MuiThemeProvider } from '@material-ui/core';
+
 import { theme } from './theme';
+import { MainMenu } from './features/main_menu/MainMenu';
 import { RaceField } from './features/race-text-field/RaceField';
 
 const App = (): JSX.Element => {
   return (
     <MuiThemeProvider theme={theme}>
       <CssBaseline />
-      <RaceField />
+      <Router>
+        <Switch>
+          <Route path='/practice'>
+            <RaceField />
+          </Route>
+          <Route path='/'>
+            <MainMenu />
+          </Route>
+          <Redirect to='/' />
+        </Switch>
+      </Router>
     </MuiThemeProvider>
   )
 };
