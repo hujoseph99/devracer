b4c103d61a06cc294d328e3cb2176d57f9c97b0e
Joseph Hu <hujoseph99@gmail.com>
2021-04-24 15:32:04 -0400

    transfering code

--- frontend/src/features/main_menu/MainMenu.tsx
+++ frontend/src/features/main_menu/MainMenu.tsx
@@ -1,5 +1,22 @@
 import React from 'react';
+import { useHistory } from 'react-router';
+
+import { Box, Button, Typography } from '@material-ui/core';
+import PersonIcon from '@material-ui/icons/Person';
 
 export const MainMenu = (): JSX.Element => {
-	return <div></div>
+	const history = useHistory();
+
+	const onPracticeClick = () => {
+		history.push('/practice');
+	}
+
+	return (
+		<Box justifyContent='center'>
+			<Typography variant='h3' color='primary'>Code Racers</Typography>
+			<Button variant='contained' endIcon={<PersonIcon />} onClick={onPracticeClick}>
+				Practice
+			</Button>
+		</Box>
+	);
 }
