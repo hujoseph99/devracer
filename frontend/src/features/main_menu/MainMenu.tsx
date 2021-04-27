10867456e7b72794ba02aa34837d1ea7cccf2eec
Joseph Hu <hujoseph99@gmail.com>
2021-04-26 23:45:17 -0400

    centered and random text

--- frontend/src/features/main_menu/MainMenu.tsx
+++ frontend/src/features/main_menu/MainMenu.tsx
@@ -1,22 +1,44 @@
 import React from 'react';
 import { useHistory } from 'react-router';
 
-import { Box, Button, Typography } from '@material-ui/core';
+import { Box, Button, makeStyles, Typography } from '@material-ui/core';
 import PersonIcon from '@material-ui/icons/Person';
+import KeyboardIcon from '@material-ui/icons/Keyboard';
+
+const useStyles = makeStyles({
+	titleIcon: {
+		fontSize: "4em",
+		marginRight: 10
+		
+	}
+});
 
 export const MainMenu = (): JSX.Element => {
 	const history = useHistory();
+	const classes = useStyles();
 
 	const onPracticeClick = () => {
 		history.push('/practice');
 	}
 
 	return (
-		<Box justifyContent='center'>
-			<Typography variant='h3' color='primary'>Code Racers</Typography>
-			<Button variant='contained' endIcon={<PersonIcon />} onClick={onPracticeClick}>
-				Practice
-			</Button>
+		<Box display='flex' flexDirection='column' justifyContent='center' alignItems='center'>
+			<Box mt={10} display='flex' justifyContent='center' alignContent='center'>
+				<KeyboardIcon className={classes.titleIcon} color='primary' />
+				<Typography component='span' variant='h3' color='primary'> 
+					CodeRacers
+				</Typography>
+			</Box>
+			<Box width='60%' mt={3}>
+				<Typography color='primary'>
+					CodeRacers is an online typing game inspired by TypeRacer. Type the most popular snippets from supported languages and increase your coding speed!
+				</Typography>
+			</Box>
+			<Box mt={5}>
+				<Button variant='contained' endIcon={<PersonIcon />} onClick={onPracticeClick}>
+					Practice
+				</Button>
+			</Box>
 		</Box>
 	);
 }
