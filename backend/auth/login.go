697b5ed4cdc2224913fab0b6a2c0da47536c010d
Joseph Hu <hujoseph99@gmail.com>
2021-04-29 22:52:36 -0400

    missed file in merge

--- backend/auth/login.go
+++ backend/auth/login.go
@@ -1,33 +1,4 @@
 
-<<<<<<< HEAD
-func (myAPI *API) loginUser(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	ctx := context.TODO()
-
-	user, err := decodeUserBody(w, r)
-	if err != nil {
-		defaultError(w, r, http.StatusBadRequest, "Invalid request.")
-		return
-	}
-
-	// check if username already exists
-	existingUser, err := myAPI.Database.FindUserByUsername(ctx, user.Username)
-	if err != nil || existingUser == nil {
-		defaultError(w, r, http.StatusUnauthorized, "The username was not found. Please try again.")
-		return
-	}
-
-	byteHash := []byte(existingUser.Password)
-	bytePassword := []byte(user.Password)
-	err = bcrypt.CompareHashAndPassword(byteHash, bytePassword)
-	if err != nil {
-		defaultError(w, r, http.StatusUnauthorized, "The password is incorrect. Please try again.")
-		return
-	}
-
-	returnUserToClient(w, r, existingUser)
-}
-=======
 const routePrefix = "/auth/"
 const githubUserURI = "https://api.github.com/user"
 
@@ -118,4 +89,3 @@ const githubUserURI = "https://api.github.com/user"
 // 	}
 // 	return contents, nil
 // }
->>>>>>> master
