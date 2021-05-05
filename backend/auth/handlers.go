1bfa21b4835c9151e17de4b8d18f11f8a0e369d1
Joseph Hu <hujoseph99@gmail.com>
2021-05-05 19:50:41 -0400

    refactored file name and helper functions

--- backend/auth/handlers.go
+++ backend/auth/handlers.go
@@ -3,57 +3,13 @@ package auth
 import (
 	"context"
 	"encoding/json"
-	"fmt"
 	"net/http"
 	"time"
 
 	"github.com/hujoseph99/typing/backend/common/api"
 	"github.com/hujoseph99/typing/backend/db"
-	"go.mongodb.org/mongo-driver/bson/primitive"
 	"golang.org/x/crypto/bcrypt"
 )
-
-// createSession creates a session for the given userid and then return thes refreshToken string
-// if successful. Otherwise, an error will be returned
-func createSession(ctx context.Context, userid primitive.ObjectID, rememberMe bool) (string, error) {
-	// create session
-	refreshToken := db.GenerateRefreshToken()
-	expiryTime := time.Now()
-	if rememberMe {
-		expiryTime = expiryTime.Add(db.RememberMeExpiryTime)
-	} else {
-		expiryTime = expiryTime.Add(db.DefaultExpiryTime)
-	}
-	session := db.NewSession(refreshToken, userid, expiryTime, rememberMe)
-	err := db.AddSession(ctx, session)
-	if err != nil {
-		return "", err
-	}
-	return refreshToken, nil
-}
-
-// updateSession will update the session containing the refresh token and then update its expiry
-// date (if it's still available). If it's not available anymore, then it will return an error.
-func updateSession(ctx context.Context, refreshToken string) (*db.SessionModel, error) {
-	session, err := db.GetSessionByRefreshToken(ctx, refreshToken)
-	if err != nil {
-		return nil, fmt.Errorf("the session does not exist")
-	}
-
-	if time.Now().After(session.ExpiryDate) {
-		return nil, fmt.Errorf("the session has expired")
-	}
-
-	newExpiry := db.GetNewExpiryTime(session.Remember)
-	session.ExpiryDate = newExpiry
-
-	err = db.UpdateSessionByRefreshToken(ctx, refreshToken, session)
-	if err != nil {
-		return nil, fmt.Errorf("there was an error when updating the refresh token")
-	}
-
-	return session, nil
-}
 
 /*
  * Expects an object like:
